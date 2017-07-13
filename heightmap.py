eb9cd0f8ebf0cd2309f3ed6d71f02584a3cb6990
Shai <shai.bianchi@mercadolibre.com>
2017-07-13 14:45:20 -0300

    Revert

--- heightmap.py
+++ heightmap.py
@@ -2,7 +2,6 @@ import multiprocessing as mp
 import numpy as np
 import geometry
 
-
 def height4position(i, j, mesh):
     samplingpoint = ((j / shared_width.value) * mesh.xspan, mesh.yspan - (i / shared_height.value) * mesh.yspan)
     containing_faces = mesh.faces_for_2D_point(samplingpoint)
@@ -21,20 +20,13 @@ def create(mesh, width, height, nprocs):
     shared_width = mp.Value('i', width)
     shared_height = mp.Value('i', height)
 
-    subregion_row = 59
-    subregion_col = 521
-    subwidth = 8
-    subheight = 8
-
-    # arguments = [(i, j, mesh) for i in range(shared_height.value) for j in range(shared_width.value)]
-    arguments = [(i, j, mesh) for i in range(subregion_row, subregion_row + subheight) for j in range(subregion_col, subregion_col + subwidth)]
+    arguments = [(i, j, mesh) for i in range(height) for j in range(width)]
 
     pool = mp.Pool(nprocs)
     result_list = list(pool.starmap_async(height4position, arguments).get())
     pool.close()
     pool.join()
 
-    # result_matrix = [ result_list[i*width:(i+1)*width] for i in range(height) ]
-    result_matrix = [ result_list[i*subwidth:(i+1)*subwidth] for i in range(subheight) ]
+    result_matrix = [ result_list[i*width:(i+1)*width] for i in range(height) ]
 
     return np.array(result_matrix)
